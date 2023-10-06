c90a7f628d71040a126db8c649b4aed1e0798af5
Carlos Rojas Vega <karlosrvxx@gmail.com>
2023-10-05 20:57:32 -0500

    update side bar admin

--- admin/admin_panel.php
+++ admin/admin_panel.php
@@ -17,47 +17,42 @@
                 <a href="#" class="hover:text-yellow-400">Inicio</a>
             </li>
             <li class="mb-2">
-            <a href="#" class="hover:text-yellow-400">Gestión de Maestros</a>
-            <ul class="ml-4 group-hover:block #">
-                <li class="hover:bg-gray-700 py-2"><a href="/admin/crear_maestro.php">Crear Maestro</a></li>
-                <li class="hover:bg-gray-700 py-2"><a href="/admin/listar_maestros.php">Listar Maestros</a></li>
-            </ul>
+                <a href="#" class="hover:text-yellow-400">Gestión de Maestros</a>
+                <ul class="ml-4 group-hover:block #">
+                    <li class="hover:bg-gray-700 py-2"><a href="/admin/crear_maestro.php">Crear Maestro</a></li>
+                    <li class="hover:bg-gray-700 py-2"><a href="/admin/listar_maestros.php">Listar Maestros</a></li>
+                </ul>
             </li>
             <li class="mb-2">
-                <a href="#" class="hover:text-yellow-400">Configuración</a>
+                <a href="#" class="hover:text-yellow-400">Gestión de Alumnos</a>
+                <ul class="ml-4 group-hover:block #">
+                    <li class="hover:bg-gray-700 py-2"><a href="/admin/crear_alumno.php">Crear Alumno</a></li>
+                    <li class="hover:bg-gray-700 py-2"><a href="/admin/listar_alumnos.php">Listar Alumno</a></li>
+                </ul>
             </li>
             <li class="mb-2">
-                <a href="#" class="hover:text-yellow-400">Reportes</a>
+                <a href="#" class="hover:text-yellow-400">Gestión de Materias</a>
+                <ul class="ml-4 group-hover:block #">
+                    <li class="hover:bg-gray-700 py-2"><a href="/admin/crear_materia.php">Crear Materia</a></li>
+                    <li class="hover:bg-gray-700 py-2"><a href="/admin/listar_materias.php">Listar materias</a></li>
+                </ul>
+            </li>
+            <li class="mb-2">
+                <a href="·" class="hover:text-yellow-400">Relacionar Maestro y Clase</a>
+                <ul class="ml-4 group-hover:block #">
+                    <li class="hover:bg-gray-700 py-2"><a href="/admin/relacionar_maestro_clase.php">Relacionar Maestro y Clase</a></li>
+                </ul>
+            </li>
+            <li class="mb-2">
+                <a href="·" class="hover:text-yellow-400">Cambio de Rol de Usuario</a>
+                <ul class="ml-4 group-hover:block #">
+                    <li class="hover:bg-gray-700 py-2"><a href="/admin/cambio_rol_usuario.php">Cambio de Rol de Usuario</a></li>
+                    <li class="hover:bg-gray-700 py-2"><a href="../cerrar_sesion.php">cerrar_sesion</a></li>
+
+                </ul>
             </li>
         </ul>
     </div>
-
-    <div>
-        <h1 class="text-3xl font-semibold mb-4">Bienvenido al Panel de Administrador</h1>
-
-        <!-- Funcionalidades para el administrador -->
-        <h2>Gestión de Maestros</h2>
-        <a href="/admin/crear_maestro.php">Crear Maestro</a>
-        <a href="/admin/listar_maestros.php">Listar Maestros</a>
-
-        <h2>Gestión de Alumnos</h2>
-        <a href="crear_alumno.php">Crear Alumno</a>
-        <a href="listar_alumnos.php">Listar Alumnos</a>
-
-        <h2>Gestión de Materias</h2>
-        <a href="/admin/crear_materia.php">Crear Materia</a>
-        <a href="/admin/listar_materias.php">Listar Materias</a>
-
-        <h2>Relacionar Maestro y Clase</h2>
-        <a href="relacionar_maestro_clase.php">Relacionar Maestro y Clase</a>
-
-        <h2>Cambio de Rol de Usuario</h2>
-        <a href="cambio_rol_usuario.php">Cambiar Rol de Usuario</a>
-
-        <br>
-        <a href="/cerrar_sesion.php">Cerrar sesión</a>
-    </div>
-
 </body>
 
 </html>
